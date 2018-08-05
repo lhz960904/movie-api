46cc5235f3712fbc587dcb397016991464e496de
lihaozecq <lihaozecq@gmail.com>
2018-08-05 13:30:42 +0800

    删除后台相关代码

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const Koa = require('koa')
 const { resolve } = require('path')
 const { connect, initSchemas } = require('./database/init')
 const R = require('ramda')
-const MIDDLEWARES = ['common', 'router'] // , 'parcel'
+const MIDDLEWARES = ['common', 'router']
 
 const useMiddlewares = (app) => {
   R.map(
