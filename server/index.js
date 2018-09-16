e981d454e268a4f912ed2d1956275196930ede66
lihaozecq <lihaozecq@gmail.com>
2018-09-16 22:42:39 +0800

    删除爬取数据相关代码

--- server/index.js
+++ server/index.js
@@ -28,13 +28,6 @@ const useMiddlewares = (app) => {
   await connect()
   // 初始化数据Schema
   initSchemas()
-  /**
-   * 爬取电影数据
-   * require('./tasks/movie')
-   * require('./tasks/api')
-   * require('./tasks/video')
-   * require('./tasks/qiniu')
-   */
   const app = new Koa()
   // 载入中间件
   await useMiddlewares(app)
