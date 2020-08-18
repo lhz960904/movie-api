54ed6b09207effec5175e5df2b7dd704487be655
lihaozecq <lihaozecq@gmail.com>
2020-08-18 15:28:08 +0800

    feat(relative movies): 获取电影详情,同时返回相似推荐

--- app/controller/movie.js
+++ app/controller/movie.js
@@ -40,8 +40,9 @@ class MovieController extends Controller {
     const { ctx, service } = this;
     const result = await service.movie.getDetail(ctx.params.id);
     if (result) {
+      const relativeMovies = await this.getRelativeList(result);
       await service.movie.update({ id: result.id, viewCount: result.viewCount + 1 })
-      ctx.sendSuccess(result)
+      ctx.sendSuccess({ movie: result, relativeMovies })
     } else {
       ctx.sendError('未查找到对应电影！')
     }
@@ -57,6 +58,19 @@ class MovieController extends Controller {
     const result = await service.movie.index({ keyword });
     ctx.sendSuccess(result)
   }
+
+  async getRelativeList({ id, movieTypes}) {
+    const categoryIds = movieTypes.map(({ id }) => id);
+    const movies = await this.service.movie.index({});
+    return movies.filter(it => it.id !== id).sort((a, b) => {
+      return this._getSimilarity(b, categoryIds) - this._getSimilarity(a, categoryIds)
+    }).slice(0, 10);
+  }
+
+  _getSimilarity(moive, types) {
+    return moive.movieTypes.reduce((a, c) => types.includes(c.id) ? a + 1 : a, 0)
+  }
+
 }
 
 module.exports = MovieController;
